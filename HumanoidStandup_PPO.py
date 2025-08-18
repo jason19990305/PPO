b06ccd915edce76fb5353a5f26b7a4f0f63b7f7f
jason19990305 <49986229+jason19990305@users.noreply.github.com>
2025-08-18 22:51:27 +0800

    Add : Humanoid Standup

---
+++ HumanoidStandup_PPO.py
@@ -0,0 +1,53 @@
+import gymnasium as gym
+import numpy as np
+import argparse
+import time
+
+from Continuous_PPO.Agent import Agent
+
+class main():
+    def __init__(self , args):
+        
+        env_name = 'HumanoidStandup-v5'
+        # Create the environment for evaluation
+        env = gym.make(env_name)
+        
+        args.num_states = env.observation_space.shape[0]
+        args.num_actions = env.action_space.shape[0]
+        args.env_name = env_name
+        print(env.action_space)
+        # Pring hyperparameters 
+        print("---------------")
+        for arg in vars(args):
+            print(arg,"=",getattr(args, arg))
+        print("---------------")
+        
+        
+        self.agent = Agent(args, env , [512,512,512]) # hidden layer size   
+        
+        self.agent.train()       
+        render_env = gym.make(env_name, render_mode="human")  
+        for i in range(1000):
+            self.agent.evaluate(render_env)
+        render_env.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Hyperparameters Setting for PPO")
+    parser.add_argument("--evaluate_freq_steps", type=float, default=2e3, help="Evaluate the policy every N environment steps")
+    parser.add_argument("--mini_batch_size", type=int, default=32, help="Mini-batch size used for PPO updates")
+    parser.add_argument("--max_train_steps", type=int, default=3e6, help="Total number of environment steps for training")
+    parser.add_argument("--use_state_norm", type=bool, default=True, help="Whether to apply state normalization")
+    parser.add_argument("--entropy_coef", type=float, default=0.00, help="Entropy bonus coefficient added to the actor loss")
+    parser.add_argument("--batch_size", type=int, default=4096, help="Number of collected samples per update (batch size)")
+    parser.add_argument("--epsilon", type=float, default=0.2, help="PPO clipping parameter (epsilon)")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of epochs to run over the batch each update")
+    parser.add_argument("--gamma", type=float, default=0.99, help="Discount factor for future rewards (gamma)")
+    parser.add_argument("--lamda", type=float, default=0.95, help="GAE lambda parameter (bias–variance trade-off)")
+    parser.add_argument("--gae", type=bool, default=False, help="Use GAE for advantage estimation")
+    parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate for the optimizers")
+    
+
+    args = parser.parse_args()
+    
+    main(args)
